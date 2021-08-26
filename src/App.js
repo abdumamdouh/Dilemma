401e088c55d1375fba59549a58358e310548dcf6
abdumamdouh <eabdogfx@gmail.com>
2021-08-26 22:49:43 +0200

    Footer Component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
+import Footer from "./components/Footer";
+
 import LoadingBar from "react-redux-loading-bar";
-
 import { handleInitialData } from "./actions/shared";
-
 import { connect } from "react-redux";
 
 class App extends Component {
@@ -14,6 +14,7 @@ class App extends Component {
       <>
         <LoadingBar />
         <h1>Hello World</h1>
+        <Footer />
       </>
     );
   }
---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Footer extends Component {
+  render() {
+    return (
+      <div className="footer">
+        <p>Dilemma, the would you rather game. </p>
+        <p>Made by Abdulrahman Mamdouh.</p>
+        <a
+          href="https://github.com/abdumamdouh"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img src="/images/github.png" alt="github"></img>
+        </a>
+        <a
+          href="https://www.linkedin.com/in/abdumamdouh/"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <img src="/images/linkedin.png" alt="linkedin"></img>
+        </a>
+      </div>
+    );
+  }
+}
+
+export default Footer;
