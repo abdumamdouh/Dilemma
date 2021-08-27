813849c9dcbe40b70964d2f79cd8d49d4513da57
abdumamdouh <eabdogfx@gmail.com>
2021-08-27 18:36:43 +0200

    leaderboard sorting logic

--- src/pages/Leaderboard.js
+++ src/pages/Leaderboard.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import Card from "../components/Card";
 
+import { connect } from "react-redux";
+
 class Leaderboard extends Component {
   render() {
+    console.log(this.props.sortedUsers);
     return (
       <div className="leaderbo">
         <Card />
@@ -11,4 +14,19 @@ class Leaderboard extends Component {
   }
 }
 
-export default Leaderboard;
+const mapStateToProps = ({ users }) => {
+  const usersArr = Object.values(users);
+
+  const sortedUsers = usersArr.sort((a, b) => {
+    const aa = Object.keys(a.answers);
+    const bb = Object.keys(b.answers);
+
+    return bb.length + b.questions.length - (aa.length + a.questions.length);
+  });
+
+  return {
+    sortedUsers,
+  };
+};
+
+export default connect(mapStateToProps)(Leaderboard);
